403b4cd99c68ce1a94e043e6aa9f7dd943b7c2b3
lounisAmazigh <lounis.berrabah@gmail.com>
2016-02-10 15:14:28 +0100

    Delete Team.py

--- tmp/Team.py
+++
@@ -1,7 +0,0 @@
-from soccersimulator import SoccerTeam
-from Strategie import *
-from soccersimulator import  Player 
-
-team1 = SoccerTeam("JSK",[Player("HAL",Test_Strategy),Player("HALL",attaque_Strategy2)])
-team2 = SoccerTeam("USMA",[Player("t2j1",Test_Strategy),Player("HAssL",attaque_Strategy2)])
-
