4a083222c3f286fc0e9104f0004b9ceddb513e70
BERRABAH LOUNIS <3503991@ppti-14-308-08.ufr-info-p6.jussieu.fr>
2016-02-22 19:47:10 +0100

    new

--- Team.py
+++ Team.py
@@ -4,6 +4,6 @@ from soccersimulator import  Player
 
 #team1 = SoccerTeam("JSK",[Player("Fonceur",milieu) , Player("Fonceur",defense_Strategy) , Player("1",goal_strat) , Player("1",goal_strat)])
 #team4 = SoccerTeam("JSK",[Player("Fonceur",defense_Strategy),Player("9",attaque_Strategy)])
-team1 = SoccerTeam("JSK",[Player("Fonceur",defense_Strategy) , Player("4",milieu)])
+team1 = SoccerTeam("JSK",[Player("Fonceur",P1_fonceur) )
 team4 = SoccerTeam("JSK",[Player("10",defense_Strategy),Player("9",attaque_Strategy),Player("4",milieu),Player("1",goal_strat)])
-team2 = SoccerTeam("JSK",[Player("Fonceur",keytest) , Player("4",defense_Strategy)])
+team2 = SoccerTeam("JSK",[Player("Fonceur",T2_All) , Player("4",defense_Strategy)])
